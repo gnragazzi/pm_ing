9a60fac2920ac390dfea262e6573af0edf8f8200
Gerardo Ragazzi <gnragazzi@gmail.com>
2024-05-27 16:04:16 -0300

    primeros frames/panels

---
+++ src/GUI/Formulario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author gera
+ */
+public class Formulario extends JPanel{
+    public Formulario(){
+        this.setBackground(Color.blue);
+        this.setPreferredSize(new Dimension(680,480));
+    }
+}
--- src/pm_ingsw1/PM_IngSw1.java
+++ src/pm_ingsw1/PM_IngSw1.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pm_ingsw1;
 
-/**
- *
- * @author gera
- */
-public class PM_IngSw1 {
+import GUI.Ventana;
 
-    /**
-     * @param args the command line arguments
-     */
+public class PM_IngSw1 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ventana principal = new Ventana();
+       
     }
     
 }
