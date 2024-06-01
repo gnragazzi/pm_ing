f94b1403e61e4f4c0fe4dbba5377347b42f9cb37
Gerardo Ragazzi <gnragazzi@gmail.com>
2024-05-31 21:58:07 -0300

    modifiqué archivos

--- src/GUI/Formulario.java
+++ src/GUI/Formulario.java
@@ -5,16 +5,37 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 /**
  *
  * @author gera
  */
 public class Formulario extends JPanel{
-    public Formulario(){
-        this.setBackground(Color.blue);
-        this.setPreferredSize(new Dimension(680,480));
+    private JPanel titulo;
+    private JPanel cuerpo;
+    public Formulario(String t){
+        titulo=new JPanel();
+        titulo.setBackground(Color.ORANGE);
+        titulo.setMaximumSize(new Dimension(6000,4000));
+        titulo.setMinimumSize(new Dimension(600,20));
+        titulo.setPreferredSize(new Dimension(600,40));
+        this.add(titulo);
+        
+        cuerpo = new JPanel();
+        cuerpo.setBackground(Color.blue);
+        
+        this.add(cuerpo);
+
+        this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
     }
 }
--- src/GUI/Ventana.java
+++ src/GUI/Ventana.java
@@ -1,9 +1,10 @@
 package GUI;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 public class Ventana extends javax.swing.JFrame {
-    public static Panel_Principal cuerpo = new Panel_Principal();
+    public Panel_Principal cuerpo = new Panel_Principal();
     
     public Ventana(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -11,7 +12,7 @@ public class Ventana extends javax.swing.JFrame {
         this.setContentPane(cuerpo);
         
         this.setMinimumSize(new Dimension(800,600));
-        this.setPreferredSize(new Dimension(800,600));
+        this.setPreferredSize(new Dimension(1200,800));
         //this.setUndecorated(true);
         this.pack();
         this.setVisible(true);
