85a91e94be89cb244decf26cb0744ce659d999b7
ezebe <ezebe@Ezequiel>
2024-06-01 00:36:41 -0300

    clases definidas

--- src/pm_ingsw1/Maquina.java
+++ src/pm_ingsw1/Maquina.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pm_ingsw1;
-
 /**
  *
  * @author ezebe
@@ -13,15 +12,17 @@ public class Maquina {
     private String Marca;
     private String Modelo;
     private Planta planta;
+    private Registro[] registro;
     private enum Estado{
         Activa,Reparación
     }
-
-    public Maquina(String NroID, String Marca, String Modelo, Planta planta) {
+    
+    public Maquina(String NroID, String Marca, String Modelo, Planta planta,Registro[] registro) {
         this.NroID = NroID;
         this.Marca = Marca;
         this.Modelo = Modelo;
         this.planta = planta;
+        this.registro=registro;
     }
 
     public String getNroID() {
@@ -55,5 +56,14 @@ public class Maquina {
     public void setPlanta(Planta planta) {
         this.planta = planta;
     }        
+
+    public Registro[] getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(Registro[] registro) {
+        this.registro = registro;
+    }
+    
     
 }
