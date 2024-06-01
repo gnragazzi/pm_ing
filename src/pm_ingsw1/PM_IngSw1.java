719792d2b774db778fab973bdf32bcd4aed9d695
ezebe <ezebe@Ezequiel>
2024-06-01 02:14:39 -0300

    clases definidas

--- src/pm_ingsw1/PM_IngSw1.java
+++ src/pm_ingsw1/PM_IngSw1.java
@@ -1,11 +1,14 @@
 package pm_ingsw1;
 
+import Connection.BD;
 import GUI.Ventana;
+import java.sql.SQLException;
 
 public class PM_IngSw1 {
-    public static void main(String[] args) {
-        Ventana principal = new Ventana();
-       
+    public static void main(String[] args) throws SQLException {
+        BD.connect();
+        System.out.println(BD.hola());
+        
     }
     
 }
